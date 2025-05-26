8cc831b86dec45b53fc8f115e3165c3af739bb13
soban-khan <soban.khanf@gmail.com>
2025-05-26 11:50:11 +0530

    Added pipes and interceptors for error messages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TYPEORM_CONFIG } from './configs/typerom.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(TYPEORM_CONFIG)],
   controllers: [AppController],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ENV } from './constants/env.constants';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './interceptors/http-exceptions.interceptor';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
+import { SwaggerModule } from '@nestjs/swagger';
+import { swaggerConfig, swaggerOptions } from './configs/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = ENV.PORT;
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.setGlobalPrefix('api/v1/');
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, swaggerDocument, swaggerOptions);
+
+  await app.listen(port);
+  console.log(`Application is running on port: ${port}`);
 }
+
 bootstrap();
